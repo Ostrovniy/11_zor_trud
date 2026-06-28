803d47c8858e178156ef0d05e40858d599b1824d
Ostrovniy <ostrovniy0001@gmail.com>
2026-06-28 15:37:05 +0300

    working with path param

--- backend/main.py
+++ backend/main.py
@@ -44,3 +44,69 @@ def read_posts():
 @app.put('/items/{item_id}')
 def test_put(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+# Path Parameters
+@app.get("/test/path-parameter/{par}")
+def test_path_parameter(par):
+    return {'value': par}
+
+# path parameters with type
+@app.get("/test/path-parameter-with-type/{par_int}")
+def test_path_parameter_with_type(par_int: int):
+    return {'value': par_int}
+
+# order matters будет перехвачивать запросы  /test/order-matters/test
+
+# order matters
+@app.get("/test/order-matters/{path_param}")
+def test_order_matters_path_param(path_param: str):
+    return {'text': f'order-matters - path_param done = {path_param}'}
+
+# order matters 2
+@app.get("/test/order-matters/test")
+def test_order_matters_test():
+    return {"text": "order-matters - test done"}
+
+
+# можно также переопределить обработку роута
+# выполняться будет пепрвый get_storages
+
+@app.get("/test/get-storages")
+def get_storages():
+    return [1, 2]
+
+@app.get("/test/get-storages")
+def get_storages2():
+    return [3, 4]
+
+
+# Когда параметр пути, являеться перечислением
+from enum import Enum
+
+# Дрпустимые параметры пути
+class ModelsName(str, Enum):
+    standart = 'standart'
+    pro = 'pro'
+    super_pro = 'super_pro'
+    
+@app.get("/test/models-name/{models_name}")
+def models_name_rout(models_name: ModelsName):
+    if models_name is ModelsName.standart:
+        return ["You get standart"]
+    
+    if models_name is ModelsName.pro:
+        return ["You get pro"]
+    
+    # Такой способ еще проверки есть
+    if models_name.value == ModelsName.super_pro.value:
+        return {"type": "super_pro", "info": "somthing"}
+    
+    return ["somthing wrong, models dont defined"]
+
+
+# Параметр пути, которыя являеться путем
+#from fastapi import Path
+@app.get("/test/path-as-path/{new_path:path}")
+def path_as_path(new_path):
+    return [f"you send path: {new_path}"]
